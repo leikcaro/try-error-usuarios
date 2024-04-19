af69b0be89e32fc2b17fd6c878e983c85a794fba
Leik P. Caro Berenguer <73296867+leikcaro@users.noreply.github.com>
2024-04-19 16:18:06 -0400

    Create usuario.py

---
+++ usuario.py
@@ -0,0 +1,7 @@
+class Usuario():
+  '''La clase co nlos datos del usuario'''
+    def __init__(self, nombre: str, apellido: str, email: str, genero: str) -> None:
+        self.nombre = nombre
+        self.apellidos = apellido
+        self.email = email
+        self.genero = genero
